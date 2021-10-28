90b98cbd15a13c8ba5d75d7a7f5180717223f1ea
mamun8893 <0316361936@student.wub.edu.bd>
2021-10-29 03:02:17 +0600

    post api create

--- index.js
+++ index.js
@@ -11,39 +11,40 @@ app.use(express.json());
 
 //Database Connection
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2jda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2jda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// async function run() {
-//   try {
-//     await client.connect();
-//     const database = client.db("volunteer");
-//     const categoriesCollection = database.collection("categories");
+async function run() {
+  try {
+    await client.connect();
+    console.log("database COnnected");
+    const database = client.db("trip_World");
+    const servicesCollection = database.collection("services");
 
-//     //Put API
+    //Put API
 
-//     app.post("/add-activity", async (req, res) => {
-//       const newActivites = req.body;
-//       const result = await categoriesCollection.insertOne(newActivites);
-//       res.json(result);
-//     });
+    app.post("/add-service", async (req, res) => {
+      const newService = req.body;
+      const result = await servicesCollection.insertOne(newService);
+      res.json(result);
+    });
 
-//     //GET API
+    //GET API
 
-//     app.get("/activity", async (req, res) => {
-//       const cursor = categoriesCollection.find({});
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-//   } finally {
-//     // await client.close()
-//   }
-// }
+    // app.get("/activity", async (req, res) => {
+    //   const cursor = categoriesCollection.find({});
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+  } finally {
+    // await client.close()
+  }
+}
 
-// run().catch(console.dir);
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
