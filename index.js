3a9b02ff88947ca656e0ecce3cf92463f9f9dd42
mamun8893 <0316361936@student.wub.edu.bd>
2021-10-29 19:49:37 +0600

    all api create

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient } = require("mongodb");
 const app = express();
 var cors = require("cors");
 require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 const port = 5000;
 
 //Middleware
@@ -22,23 +23,57 @@ async function run() {
     await client.connect();
     console.log("database COnnected");
     const database = client.db("trip_World");
-    const servicesCollection = database.collection("services");
+    const packageCollection = database.collection("package");
+    const bookPackageCollection = database.collection("book_package");
 
     //Put API
 
     app.post("/add-service", async (req, res) => {
       const newService = req.body;
-      const result = await servicesCollection.insertOne(newService);
+      const result = await packageCollection.insertOne(newService);
+      res.json(result);
+    });
+
+    //Book package Put API
+
+    app.post("/book-package", async (req, res) => {
+      const newBookPackage = req.body;
+      const result = await bookPackageCollection.insertOne(newBookPackage);
       res.json(result);
     });
 
     //GET API
 
-    // app.get("/activity", async (req, res) => {
-    //   const cursor = categoriesCollection.find({});
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
+    app.get("/package", async (req, res) => {
+      const cursor = packageCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //Get Single Item API
+    app.get("/package/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const package = await packageCollection.findOne(query);
+      res.send(package);
+    });
+
+    //Get My Order API
+
+    app.get("/myOrders/:email", async (req, res) => {
+      const userEmail = req.params.email;
+      const result = await bookPackageCollection
+        .find({ email: userEmail })
+        .toArray();
+      res.json(result);
+    });
+
+    app.delete("/orderDelete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookPackageCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close()
   }
